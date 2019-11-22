a169b433f45c2358c030f4967bc71573f61a81e3
charantej224 <charantej.career@gmail.com>
2019-11-21 20:21:59 -0600

    Question2 changes

--- workspace/python_workspace/apps/Question2.py
+++ workspace/python_workspace/apps/Question2.py
@@ -1,5 +1,5 @@
 from pyspark.sql import *
-from pyspark.sql.types import DoubleType, IntegerType
+from pyspark.sql.types import IntegerType
 
 ## a. Import the dataset and create df and print Schema
 spark = SparkSession \
@@ -35,21 +35,25 @@ worldcup_matches.printSchema()
 # 3 Matches from 1950
 worldcup_matches.filter(worldcup_matches.Year.like("1950")).show()
 
-# 4 Count the total no. of matches played in 2014
-print("Matches in 2014 : " + str(worldcup_matches.filter(worldcup_matches.Year.like("2014")).count()))
+# 4 Count the total no. of matches played in 1934 && round like preliminary
+print("Matches in 1934 : " + str(worldcup_matches.filter(worldcup_matches.Year.like("1934") & worldcup_matches.Round.like("Preliminary round")).count()))
 
-# 5 Count the no.of times Argetina has appeared in semi-finals
+# 5 Count the no.of times Argentina has appeared in semi-finals
 worldcup_matches.filter(
-    worldcup_matches.Round.like("Semi-finals") & (worldcup_matches.Home_Team == 'Argentina')).count()
+    worldcup_matches.Round.like("Semi-finals") & (worldcup_matches.Home_Team.isin('Argentina','Brazil'))).count()
 
-# 6 Display all the finale matches
+# 6 Print final matches from all world cups.
 worldcup_matches.filter(worldcup_matches.Round == 'Final').show()
+
 # 7 pairing stage wise matches with home team
 worldcup_matches.crosstab('Round', 'Home_Team').show()
+
 # 8 finding mean, max, min etc of the attendance over the entire matches
 worldcup_matches.describe(['Away_Team_Goals']).show()
+
 # 9 Display the max away goals scored year wise
 worldcup_matches.groupBy('Year').max('Away_Team_Goals').dropna().show()
+
 # 10 Display the sum of home goals scored, grouped by each of the each team country
 worldcup_matches.groupBy('Home_Team').sum('Home_Team_Goals').orderBy('Home_Team').dropna().show(15, truncate=True)
 
--- workspace/python_workspace/apps/Question2_2.py
+++ workspace/python_workspace/apps/Question2_2.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     world_cup_content = world_cup_rdd.filter(lambda line: line != world_cup_header)
 
     # creating an RDD
-    rdd = world_cup_content.map(lambda line: (line.split(","))).collect()
+    rdd_for_dataframe = world_cup_content.map(lambda line: (line.split(","))).collect()
     # print(rdd)
     # no. of Columns
     rdd_len = world_cup_content.map(lambda line: len(line.split(","))).distinct().collect()
@@ -43,16 +43,21 @@ if __name__ == "__main__":
                                    StructField('QualifiedTeams', StringType(), True),
                                    StructField('MatchesPlayed', StringType(), True),
                                    StructField('Attendance', StringType(), True)])
+
     # Create data frame from the RDD
-    df = spark.createDataFrame(rdd, dataframe_schema)
-    df.show()
-    df = df.withColumn('GoalsScored', df['GoalsScored'].cast(IntegerType()))
-    df = df.withColumnRenamed('Runners-Up', 'Runnersup')
+    world_cup_dataframe = spark.createDataFrame(rdd_for_dataframe, dataframe_schema)
+    world_cup_dataframe.show()
+    world_cup_dataframe = world_cup_dataframe.withColumn('GoalsScored', world_cup_dataframe['GoalsScored'].cast(IntegerType()))
+    world_cup_dataframe = world_cup_dataframe.withColumnRenamed('Runners-Up', 'Runnersup')
+
+    # Registering Temp table.
+    world_cup_dataframe.createOrReplaceTempView("worldcup_table")
+
     # venue - hosted country with highest goals (From DF)
-    df.select("Country", "GoalsScored").orderBy("GoalsScored", ascending=False).show(20, truncate=False)
+    world_cup_dataframe.select("Country", "GoalsScored").orderBy("GoalsScored", ascending=False).show(20, truncate=False)
     # venue - hosted country with highest goals (From DF - SQL)
-    df.createOrReplaceTempView("df_table")
-    spark.sql(" SELECT Country,GoalsScored FROM df_table order by " +
+
+    spark.sql(" SELECT Country,GoalsScored FROM worldcup_table order by " +
               "GoalsScored Desc Limit 10").show()
 
     print("---------- 2. Year, venue country = winning country ------------")
@@ -61,9 +66,9 @@ if __name__ == "__main__":
      .map(lambda line: (line.split(",")[0], line.split(",")[1], line.split(",")[2]))
      .collect())
     # using DF
-    df.select("Year", "Country", "Winner").filter(df["Country"] == df["Winner"]).show()
+    world_cup_dataframe.select("Year", "Country", "Winner").filter(world_cup_dataframe["Country"] == world_cup_dataframe["Winner"]).show()
     # using DF - SQL
-    spark.sql(" SELECT Year,Country,Winner FROM df_table where Country == Winner order by Year").show()
+    spark.sql(" SELECT Year,Country,Winner FROM worldcup_table where Country == Winner order by Year").show()
 
     print("-------------- 3. Details of years ending in ZERO ---------------")
     # using RDD
@@ -71,10 +76,10 @@ if __name__ == "__main__":
     (world_cup_content.filter(lambda line: line.split(",")[0] in years)
      .map(lambda line: (line.split(",")[0], line.split(",")[2], line.split(",")[3])).collect())
     # using DF
-    df.select("Year", "Winner", "Runnersup").filter(df.Year.isin(years)).show()
+    world_cup_dataframe.select("Year", "Winner", "Runnersup").filter(world_cup_dataframe.Year.isin(years)).show()
     # using DF - SQL
 
-    spark.sql(" SELECT Year,Winner,Runnersup FROM df_table  WHERE " +
+    spark.sql(" SELECT Year,Winner,Runnersup FROM worldcup_table  WHERE " +
               " Year IN ('1930','1950','1970','1990','2010') ").show()
 
     print("-------------- 4. 2014 world cup stats --------------")
@@ -83,17 +88,18 @@ if __name__ == "__main__":
     (world_cup_content.filter(lambda line: line.split(",")[0] == "2014")
      .map(lambda line: (line.split(","))).collect())
     # using DF
-    df.filter(df.Year == "2014").show()
+    world_cup_dataframe.filter(world_cup_dataframe.Year == "2014").show()
     # using DF - Sql
-    spark.sql(" Select * from df_table where Year == 2014 ").show()
+    spark.sql(" Select * from worldcup_table where Year == 2014 ").show()
 
     print("------------- 5. Max matches played -----------------")
     # Using RDD
     (world_cup_content.filter(lambda line: line.split(",")[8] == "64")
      .map(lambda line: (line.split(","))).collect())
     # using DF
-    df = df.withColumn('MatchesPlayed', df['MatchesPlayed'].cast(IntegerType()))
-    df.filter(df.MatchesPlayed == 64).show()
+    world_cup_dataframe = world_cup_dataframe.withColumn('MatchesPlayed', world_cup_dataframe['MatchesPlayed'].cast(IntegerType()))
+    world_cup_dataframe.filter(world_cup_dataframe.MatchesPlayed == 64).show()
+
     # using DF - SQL
-    spark.sql(" Select * from df_table where MatchesPlayed in " +
-              "(Select Max(MatchesPlayed) from df_table )").show()
+    spark.sql(" Select * from worldcup_table where MatchesPlayed in " +
+              "(Select Max(MatchesPlayed) from worldcup_table )").show()
